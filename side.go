78652fdaf8becef4eff965a7e713145b71dc36e7
Titus T <dxtitus@gmail.com>
2024-02-05 08:24:22 +0000

    added go mod 1.21

--- side.go
+++ side.go
@@ -1,70 +1,70 @@
 package side
 
 const (
-	Left  = -1 + iota
+	Left = -1 + iota
 	On
 	Right
 )
 
-//type Side of zero
+// Side type
 type Side struct {
 	s int
 }
 
-//New Side
+// NewSide - Create New Side
 func NewSide() *Side {
 	return &Side{On}
 }
 
-//get value
+// Value - get value
 func (side *Side) Value() int {
 	return side.s
 }
 
-//is left
+// IsLeft - is left
 func (side *Side) IsLeft() bool {
 	return side.s == Left
 }
 
-//as left
+// AsLeft - as left
 func (side *Side) AsLeft() *Side {
 	side.s = Left
 	return side
 }
 
-//Is on
+// IsOn - Is on
 func (side *Side) IsOn() bool {
 	return side.s == On
 }
 
-//As on
+// AsOn - As on
 func (side *Side) AsOn() *Side {
 	side.s = On
 	return side
 }
 
-//Is right
+// IsRight - Is right
 func (side *Side) IsRight() bool {
 	return side.s == Right
 }
 
-//As right
+// AsRight - As right
 func (side *Side) AsRight() *Side {
 	side.s = Right
 	return side
 }
 
-//is on or left
+// IsOnOrLeft - is on or left
 func (side *Side) IsOnOrLeft() bool {
 	return side.IsOn() || side.IsLeft()
 }
 
-//is on or right
+// IsOnOrRight - is on or right
 func (side *Side) IsOnOrRight() bool {
 	return side.IsOn() || side.IsRight()
 }
 
-//is on the same side
+// IsSameSide - is on the same side
 func (side *Side) IsSameSide(other *Side) bool {
 	return side.s == other.s
 }
